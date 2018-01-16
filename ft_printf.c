cc90db9159cc0a71651a7a15e2bef3fef524b37a
Vika <viktoria.likhotkina@gmail.com>
2018-01-16 22:14:01 +0200

    Add i, u, C, S types, + flag

--- ft_printf.c
+++ ft_printf.c
@@ -8,57 +8,86 @@
 	//the precision
 	// flags hh, h, l, ll, j, et z.
 
+void check_flags(char flag)
+{
+	if (flag == '+')
+		write(1, "+", 1);
+}
+
+
 int ft_printf(const char *format, ...)
 {
 
 	int i = 0;
 	va_list ap;
+	char *p;
+	int j = 0;
+	p = (char *)format;
 	va_start(ap, format);
 	int d;
-  char c, *s;
-  printf("format %s\n", format);
-	while (format[i])
+  	char a, c, *s;
+  	wchar_t v, *w;
+	while (p[i])
 	{
-		while (format[i] != '%' && format[i])
+		while (p[i] != '%' && p[i])
 		{
-			write(1, &format[i], 1);
+			write(1, &p[i], 1);
 			i++;
 		}
-		if(format[i] == '%')
+		if (p[i] == '%')
 		{
 			i++;
-			if(format[i] == '%')
+			if (p[i] == '+')
 			{
-				c = va_arg(ap, int);
-				write(1, &c, 1);
+				if (p[i + 1] == 'd' || p[i + 1] == 'i')
+				write(1, &p[i], 1);
 				i++;
 			}
-			if(format[i] == 's')
+			if(p[i] == 's')
 			{
 				s = va_arg(ap, char *);
 				write(1, s, ft_strlen(s));
 			}
-			if(format[i] == 'd')
+			else if(p[i] == 'd' || p[i] == 'i' || p[i] == 'u')
 			{
 				d = va_arg(ap, int);
 				ft_putnbr(d);
 			}
-			if(format[i] == 'c')
+			else if(p[i] == 'c')
 			{
 				c = va_arg(ap, int);
-	      write(1, &c, 1);
+      			write(1, &c, 1);
 			}
+			else if(p[i] == 'C')
+			{
+				v = va_arg(ap, wchar_t);
+      			write(1, &v, 1);
+			}
+			else if(p[i] == 'S')
+			{
+				w = va_arg(ap, wchar_t *);
+				j = 0;
+				while (w[j])
+				{
+					write(1, &w[j], 1);
+					j += 1;
+				}
+			}
+			else
+				write(1, &p[i], 1);
 			i++;
-		}
-	}
+			}
+		}	
 	va_end(ap);
-	return (ft_strlen(format));
+	return (ft_strlen(p));
 }
 
 int main(void)
 {
-	ft_printf("%s%c", "d", 's');
-	printf("\n%s%c", "d", 's');
-	//printf("%d\n", printf("intыфваывавыа%d\n", 12));
+	printf("real   %%%s dsfdsfdsf %c %+d %+i %u %S\n", "string", 'c', 156, 651, 54646, L"abcdef");
+	ft_printf("custom %%%s dsfdsfdsf %c %+d %+i %u %S\n", "string", 'c', 156, 651, 54646, L"abcdef");
+	printf("real   %C\n", 't');
+	ft_printf("custom %C\n", 't');
+	
 	return (0);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -16,6 +16,7 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdarg.h>
+# include "libft/libft.h"
 
 int ft_printf(const char *format, ...);
 size_t		ft_strlen(const char *s);
